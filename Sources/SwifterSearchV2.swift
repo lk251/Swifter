c33080fa2d46563d358d846d22b3042af836ef8b
levkusanagi <21109904+lev-kusanagi@users.noreply.github.com>
2020-12-13 14:47:55 +0100

    searchv2 uses .api2 baseURL

--- Sources/SwifterSearchV2.swift
+++ Sources/SwifterSearchV2.swift
@@ -67,7 +67,7 @@ public extension Swifter {
         parameters["callback"] ??= callback
         parameters["tweet_mode"] ??= tweetMode.stringValue
 
-        self.getJSON(path: path, baseURL: .api, parameters: parameters, success: { json, _ in
+        self.getJSON(path: path, baseURL: .api2, parameters: parameters, success: { json, _ in
             success?(json["statuses"], json["search_metadata"])
             }, failure: failure)
     }
