b81d1966c52441a15f49da9b025fc13cf4fab5db
levkusanagi <21109904+lev-kusanagi@users.noreply.github.com>
2021-02-03 12:38:00 +0100

    add pagination token to search request

--- Sources/SwifterSearchV2.swift
+++ Sources/SwifterSearchV2.swift
@@ -46,6 +46,7 @@ public extension Swifter {
                      locale: String? = nil,
                      resultType: String? = nil,
                      maxResults: Int? = nil,
+                     paginationToken: String? = nil,
                      until: String? = nil,
                      sinceID: String? = nil,
                      maxID: String? = nil,
@@ -66,6 +67,7 @@ public extension Swifter {
         parameters["locale"] ??= locale
         parameters["result_type"] ??= resultType
         parameters["max_results"] ??= maxResults
+        parameters["pagination_token"] ??= paginationToken
         parameters["until"] ??= until
         parameters["since_id"] ??= sinceID
         parameters["max_id"] ??= maxID
